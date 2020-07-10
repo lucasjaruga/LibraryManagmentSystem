d011297aa7edde6649f8c98979970a0fa0171767
Lucas Jaruga <lucas.jaruga@outlook.com>
2020-07-11 00:56:13 +0200

    added BookController and some learning about Spring MVC

---
+++ src/main/java/pl/jarugalucas/libraryManagmentSystem/controllers/BookController.java
@@ -0,0 +1,28 @@
+package pl.jarugalucas.libraryManagmentSystem.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import pl.jarugalucas.libraryManagmentSystem.repositories.BookRepository;
+
+@Controller
+public class BookController {
+	
+	private final BookRepository bookRepository;
+	
+	public BookController(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+		// injection of bookRepository to BookController
+	}
+	
+	// when Spring gets request for /books url, this will execute getBooks method
+	@RequestMapping("/books")
+	public String getBooks(Model model) {
+		
+		// adds all books to the model and next it'll return model to view layer
+		model.addAttribute("book", bookRepository.findAll());
+		
+		return "books";
+	}
+}
